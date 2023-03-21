c9361e422bae96626774345bdd83d5fdc687a3e8
Aleksandr <alexbachurin91@gmail.com>
2023-03-22 01:31:29 +0300

    check auth headers to be present when accessing private route and get token from frontend,decode on backend and display private route with data info and username

--- controllers/main.js
+++ controllers/main.js
@@ -18,11 +18,27 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const randomNum = Math.floor(Math.random() * 100);
-  res.status(200).json({
-    msg: `Hello, John Doe`,
-    secret: `Here is your auth data, your lucky number is ${randomNum}`,
-  });
+  // get authtorization header from request headers
+  const authHeader = req.headers.authorization;
+  // if auth headers not provided throw error
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("Invalid credentials", 401);
+  }
+  //   extract token from string which will be `Bearer token`
+  // so split string into array and get token from second position
+  const token = authHeader.split(" ")[1];
+
+  try {
+    // decode jwt token to extract username
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const randomNum = Math.floor(Math.random() * 100);
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}`,
+      secret: `Here is your auth data, your lucky number is ${randomNum}`,
+    });
+  } catch (error) {
+    throw new CustomAPIError("Not auhtorized to access this route", 401);
+  }
 };
 
 module.exports = { login, dashboard };
