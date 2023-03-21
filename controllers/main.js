3bea6fa7d1b29a5f26b6f5ca50e303752683b84c
Aleksandr <alexbachurin91@gmail.com>
2023-03-22 01:14:11 +0300

    project setup+create token on login with jwt sign

---
+++ controllers/main.js
@@ -0,0 +1,28 @@
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  //   validation in the controller, check for username and password
+  //   if they are not provided then throw our custom API error
+  if (!username || !password) {
+    throw new CustomAPIError("username and password should be provided", 400);
+  }
+  //create token
+  //keep payload small for better user experience
+  const id = new Date().getTime();
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "user created", token });
+};
+
+const dashboard = async (req, res) => {
+  const randomNum = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, John Doe`,
+    secret: `Here is your auth data, your lucky number is ${randomNum}`,
+  });
+};
+
+module.exports = { login, dashboard };
